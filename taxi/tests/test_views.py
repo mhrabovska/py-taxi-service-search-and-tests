674ed24252ed7f6ab9746e9310f76f51812080ff
Maria Hrabovska <mariya76om@gmail.com>
2025-07-22 22:19:03 +0300

    fix: valid success_url for DriverDeleteView + added screenshots to PR

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from taxi.models import Manufacturer, Car, Driver
 from django.contrib.auth import get_user_model
 
+
 class ListViewSearchTests(TestCase):
     def setUp(self):
         # Створимо користувача для входу
@@ -12,11 +13,23 @@ class ListViewSearchTests(TestCase):
         self.client.login(username="testuser", password="testpass")
 
         # Дані для тестів
-        self.manufacturer1 = Manufacturer.objects.create(name="Toyota", country="Japan")
-        self.manufacturer2 = Manufacturer.objects.create(name="Ford", country="USA")
+        self.manufacturer1 = Manufacturer.objects.create(
+            name="Toyota",
+            country="Japan"
+        )
+        self.manufacturer2 = Manufacturer.objects.create(
+            name="Ford",
+            country="USA"
+        )
 
-        self.car1 = Car.objects.create(model="Corolla", manufacturer=self.manufacturer1)
-        self.car2 = Car.objects.create(model="Focus", manufacturer=self.manufacturer2)
+        self.car1 = Car.objects.create(
+            model="Corolla",
+            manufacturer=self.manufacturer1
+        )
+        self.car2 = Car.objects.create(
+            model="Focus",
+            manufacturer=self.manufacturer2
+        )
 
         self.driver1 = Driver.objects.create(
             username="driver_one", password="pass", license_number="ABC123"
@@ -43,7 +56,7 @@ class ListViewSearchTests(TestCase):
     def test_search_driver_by_username(self):
         response = self.client.get(reverse(
             "taxi:driver-list") + "?username=driver_one"
-                                   )
+        )
 
         self.assertContains(response, "driver_one")
         self.assertNotContains(response, "anotherdriver")
